79dad5b9e6c7c140d5eb04be7426e38342a98328
Sefayet <sefayetalam14@gmail.com>
2024-08-28 23:56:39 +0600

    update live exam

---
+++ Module 15/Live_Exam/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+const PORT = 8000;
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './uploads');
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage }).single('myfile'); //postman key=myfile
+
+//route: http://localhost:8000/upload***
+app.post('/upload', function (req, res) {
+    upload(req, res, function (error) {
+        if (error) {
+            return res.send("Error uploading file..");
+        }
+        else {
+            res.send('File uploaded successfully!');
+        }
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+//github link: 
