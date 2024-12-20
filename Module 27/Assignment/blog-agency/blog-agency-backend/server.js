89081e3a42595f46e37f61923a5cd33fa41ffb65
Sefayet-Alam <sefayetalam14@gmail.com>
2024-12-21 02:18:06 +0600

    update all

--- Module 27/Assignment/blog-agency/blog-agency-backend/server.js
+++ Module 27/Assignment/blog-agency/blog-agency-backend/server.js
@@ -25,7 +25,7 @@ app.use('/api/services', serviceRoutes);
 app.use('/api/teams', teamRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
